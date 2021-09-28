d70ba9f8241fdbcf5b1f98390aa61073cdead074
GriffinCogghe <90715750+GriffinCogghe@users.noreply.github.com>
2021-09-28 13:20:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Programmer: Griffin Cogghe
+# Date: Sept 28 2021
+# Description: find the average of the 3 numbers
+
+integer_1 = int(input("Enter an integer: "))
+integer_2 = int(input("Enter another integer: "))
+integer_3 = int(input("Enter one more integer: "))
+
+average = integer_1 + integer_2 + integer_3 / 3
+
+print()
+print(f"The average is {average}.")
+
+
